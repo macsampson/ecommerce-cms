c065054e44b005ddaf690e389a3fe9d6a02885b5
Mac Sampson <mac.sampson@proton.me>
2022-11-27 12:00:00 -0800

    remove unused cors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/**  @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
@@ -6,19 +6,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-// next.config.js
-// module.exports = {
-//   async headers() {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         headers: [
-//           { key: "Access-Control-Allow-Origin", value: "*" },
-//           { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-//           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }
-//         ],
-//       },
-//     ]
-//   },
-// }

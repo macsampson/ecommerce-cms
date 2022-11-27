8df98d35a1f022217eaf009841eacf76a3fea841
Mac Sampson <mac.sampson@proton.me>
2022-11-27 12:00:00 -0800

    update dashboard with live sales data

--- next.config.js
+++ next.config.js
@@ -6,3 +6,19 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// next.config.js
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+//           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }
+//         ],
+//       },
+//     ]
+//   },
+// }

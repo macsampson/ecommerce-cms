369c3c2604e0e6f9b4937eea0826da2b0508299c
Mac Sampson <mac.sampson@proton.me>
2022-11-27 03:22:34 -0800

    add clerk authentication

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useEffect } from "react"
+import { useStoreModal } from "@/hooks/use-store-modal"
+
+const SetupPage = () => {
+  const isOpen = useStoreModal((state) => state.isOpen)
+  const onOpen = useStoreModal((state) => state.onOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
+  return <div className="p-4">Root page</div>
+}
+
+export default SetupPage

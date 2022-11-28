5ad57fcf8f1a5a55bcd2cccf91e6a2c3e967e93a
Mac Sampson <mac.sampson@proton.me>
2022-11-28 10:25:30 -0800

    add main nav and comps

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -19,6 +19,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { toast } from "react-hot-toast"
+import { redirect } from "next/navigation"
 
 const formSchema = z.object({
   name: z.string().min(1, "Store name must be at least 1 character long."),
@@ -41,7 +42,7 @@ export const StoreModal = () => {
       setLoading(true)
       const response = await axios.post("/api/stores", values)
 
-      toast.success("Store created.")
+      window.location.assign(`/${response.data.id}`)
     } catch (error) {
       toast.error("Something went wrong.")
     } finally {

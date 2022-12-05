e08083fa7342c73d0813b64c9124f18b6eead8c0
Mac Sampson <mac.sampson@proton.me>
2022-12-04 19:42:55 -0800

    make color and size optional

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -39,8 +39,8 @@ const formSchema = z.object({
   images: z.object({ url: z.string() }).array(),
   price: z.coerce.number().min(1),
   categoryId: z.string().min(1),
-  colorId: z.string().min(1),
-  sizeId: z.string().min(1),
+  colorId: z.string().optional(),
+  sizeId: z.string().optional(),
   isFeatured: z.boolean().default(false).optional(),
   isArchived: z.boolean().default(false).optional(),
 })
@@ -79,14 +79,16 @@ export const ProductForm: React.FC<ProductFormProps> = ({
     ? {
         ...initialData,
         price: parseFloat(String(initialData?.price)),
+        colorId: initialData.colorId || undefined,
+        sizeId: initialData.sizeId || undefined,
       }
     : {
         name: "",
         images: [],
         price: 0,
         categoryId: "",
-        colorId: "",
-        sizeId: "",
+        colorId: undefined,
+        sizeId: undefined,
         isFeatured: false,
         isArchived: false,
       }
@@ -262,7 +264,9 @@ export const ProductForm: React.FC<ProductFormProps> = ({
               name="sizeId"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Size</FormLabel>
+                  <FormLabel>
+                    Sizes <span className="text-neutral-500">(optional)</span>
+                  </FormLabel>
                   <Select
                     disabled={loading}
                     onValueChange={field.onChange}
@@ -297,7 +301,9 @@ export const ProductForm: React.FC<ProductFormProps> = ({
               name="colorId"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Color</FormLabel>
+                  <FormLabel>
+                    Color <span className="text-neutral-500">(optional)</span>
+                  </FormLabel>
                   <Select
                     disabled={loading}
                     onValueChange={field.onChange}

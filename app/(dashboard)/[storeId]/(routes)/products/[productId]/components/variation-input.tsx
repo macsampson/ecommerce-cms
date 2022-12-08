971dea69d42ddd775ef0ea8a61275016d76721a1
Mac Sampson <mac.sampson@proton.me>
2022-12-08 09:36:51 -0800

    display number on focus and dollars on blur

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/variation-input.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/variation-input.tsx
@@ -1,46 +1,64 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { formatter } from "@/lib/utils"
-// import { ProductVariation } from "@prisma/client"
 
-import React from "react"
+import { useState } from "react"
 
 interface VariationInputProps {
   variation: { name: string; price: number }
   onRemove: () => void
   onVariationUpdate: (name: string, value: string) => void
-  //   updateVariation: (key: number, value: string) => void
-  //   key: number
 }
 
 const VariationInput: React.FC<VariationInputProps> = ({
   variation,
   onRemove,
   onVariationUpdate,
-  //   updateVariation,
-  //   key,
 }) => {
   const name = variation?.name || ""
-  const displayPrice =
-    variation?.price && variation.price > 0 ? variation.price : ""
+  const displayPrice = formatter.format(variation?.price || 0)
+
+  const [price, setPrice] = useState(variation?.price.toString())
+  const [isPriceFocused, setIsPriceFocused] = useState(false)
+
+  const formattedPrice = isPriceFocused ? price : displayPrice
+
+  // function to prevent default behavior of the input when the user presses enter
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault()
+      //blur the input to trigger the onBlur event
+      e.currentTarget.blur()
+    }
+  }
 
   return (
-    <div className="flex items-center space-x-4 justify-between w-full px-2">
+    <div className="flex items-center space-x-4 justify-between w-full">
       <Input
-        className=" text-sm font-medium w-1/2"
+        className=" text-sm font-medium w-4/5"
         placeholder="Name"
         value={name}
         onChange={(e) => {
           onVariationUpdate("name", e.target.value)
         }}
+        onKeyDown={handleKeyDown}
       />
       <Input
-        className="text-sm font-medium text-right w-1/2 "
+        className="text-sm font-medium text-right w-1/5 "
         placeholder="$0.00"
-        value={variation?.price}
-        onChange={(e) => {
-          onVariationUpdate("price", e.target.value)
+        value={formattedPrice}
+        onFocus={() => {
+          setIsPriceFocused(true)
         }}
+        onChange={(e) => {
+          setPrice(e.target.value)
+        }}
+        onBlur={() => {
+          setIsPriceFocused(false)
+          const numberValue = parseFloat(price.replace(/[^0-9.-]+/g, ""))
+          onVariationUpdate("price", numberValue.toString())
+        }}
+        onKeyDown={handleKeyDown}
       />
       <Button
         type="button"

2ff36d344f786b2784bf7c37e43bc08269d848ab
Mac Sampson <mac.sampson@proton.me>
2023-12-18 16:19:06 -0800

    add shippo transaction to checkout

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,6 +5,8 @@ import { NextResponse } from "next/server"
 import { stripe } from "@/lib/stripe"
 import prismadb from "@/lib/prismadb"
 
+import shippoClient from "@/lib/shippo"
+
 type ItemsObject = {
   [key: string]: number
 }
@@ -26,6 +28,8 @@ export async function POST(req: Request) {
   }
 
   const session = event.data.object as Stripe.Checkout.Session
+  const currency = session?.currency
+
   const address = session?.customer_details?.address
 
   const addressComponents = [
@@ -53,6 +57,47 @@ export async function POST(req: Request) {
         orderItems: true,
       },
     })
+
+    // const order = await prismadb.order.create({
+    //   data: {
+    //     storeId: params.storeId,
+    //     isPaid: false, // set true as per your payment logic
+    //     totalPrice: Object.entries(items).reduce((total, [_, item]) => {
+    //       return total + item.price
+    //     }, 0),
+    //     orderItems: {
+    //       create: Object.entries(items).flatMap(([productId, item]) => {
+    //         // If the item is a bundle, create an orderItem for each variation
+    //         if (Object.keys(item.variations).length > 0) {
+    //           return Object.entries(item.variations).map(
+    //             ([variationId, variation]) => ({
+    //               product: { connect: { id: productId } },
+    //               price: variation.price,
+    //               quantity: variation.quantity, // Quantity per variation
+    //               productVariation: { connect: { id: variationId } },
+    //             })
+    //           )
+    //         } else {
+    //           // If the item is not a bundle, create a single orderItem
+    //           return [
+    //             {
+    //               product: { connect: { id: productId } },
+    //               price: item.price,
+    //               quantity: item.quantity,
+    //             },
+    //           ]
+    //         }
+    //       }),
+    //     },
+    //   },
+    //   include: {
+    //     orderItems: {
+    //       include: {
+    //         bundleItems: true,
+    //       },
+    //     },
+    //   },
+    // })
 
     // const productIds = order.orderItems.map((orderItem) => orderItem.productId)
     const productsObject = order.orderItems.reduce(
@@ -85,6 +130,26 @@ export async function POST(req: Request) {
           },
         },
       })
+    }
+
+    // TODO: create shipment with shippo
+    const shippingID = session.shipping_cost?.shipping_rate
+
+    const shippingRate = await stripe.shippingRates.retrieve(
+      shippingID as string
+    )
+    // console.log("retrieved from stripe: ", shippingRate)
+    // console.log("shipping id: ", shippingRate.metadata.id)
+
+    try {
+      const transaction = shippoClient.transaction.create({
+        rate: shippingRate.metadata.id,
+        label_file_type: "PDF",
+        async: false,
+      })
+      console.log(transaction)
+    } catch (error) {
+      console.log(error)
     }
   }
 

87e802f15b6b279717b2959a7fc554459df12473
Mac Sampson <mac.sampson@proton.me>
2023-12-18 16:20:40 -0800

    improve order dash format so variations are listed under product, and shipping address is included

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -28,50 +28,57 @@ const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
     },
   })
 
-  const formattedOrders: OrderColumn[] = orders.map((order) => ({
-    id: order.id,
-    phone: order.phone,
-    address: order.address,
-    // for each product, if it has bundle items, then show the product name and the bundle items
-    // otherwise, show the product name and the product variation name
-    // join all of these with a comma
-    products: order.orderItems
-      .map((item) => {
-        if (item.bundleItems.length > 0) {
-          return `${item.product.name} (${item.bundleItems
-            .map(
-              (bundleItem) =>
-                `${bundleItem.quantity}x ${bundleItem.productVariation.name}`
-            )
-            .join(", ")})`
-        } else {
-          return `${item.product.name} (${
-            item.productVariation ? item.productVariation.name : "Standard"
-          })`
-        }
+  const formattedOrders: OrderColumn[] = orders.map((order) => {
+    const productGroups: { [key: string]: string[] } = {}
+    order.orderItems.forEach((item) => {
+      const productName = item.product.name
+      if (!productGroups[productName]) {
+        productGroups[productName] = []
+      }
+
+      const variationName = item.productVariation
+        ? item.productVariation.name
+        : "Standard"
+      productGroups[productName].push(`- ${variationName} x${item.quantity}`)
+    })
+
+    const productString = Object.entries(productGroups)
+      .map(([productName, variations]) => {
+        return `${productName}\n ${variations.join("\n ")}`
       })
-      .join(" | "),
-    variations: order.orderItems
-      .map((item) => {
-        if (item.bundleItems.length > 0) {
-          return `${item.product.name} (${item.bundleItems
-            .map((bundleItem) => bundleItem.productVariation.name)
-            .join(", ")})`
-        } else {
-          return `${item.product.name} (${
-            item.productVariation ? item.productVariation.name : "Standard"
-          })`
-        }
-      })
-      .join(", "),
-    totalPrice: formatter.format(order.totalPrice.toNumber()),
-    isPaid: order.isPaid,
-    createdAt: format(order.createdAt, "MMMM do, yyyy"),
-  }))
+      .join("\n\n")
+
+    return {
+      id: order.id,
+      phone: order.phone,
+      address: order.address,
+      shippingAddress: order.shippingAddress,
+      // for each product, if it has bundle items, then show the product name and the bundle items
+      // otherwise, show the product name and the product variation name
+      // join all of these with a comma
+      products: productString,
+      variations: order.orderItems
+        .map((item) => {
+          if (item.bundleItems.length > 0) {
+            return `${item.product.name} (${item.bundleItems
+              .map((bundleItem) => bundleItem.productVariation.name)
+              .join(", ")})`
+          } else {
+            return `${item.product.name} (${
+              item.productVariation ? item.productVariation.name : "Standard"
+            })`
+          }
+        })
+        .join(", "),
+      totalPrice: formatter.format(order.totalPrice.toNumber()),
+      isPaid: order.isPaid,
+      createdAt: format(order.createdAt, "MMMM do, yyyy"),
+    }
+  })
 
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex-1 space-y-4 p-8 pt-6 whitespace-pre-wrap">
         <OrderClient data={formattedOrders} />
       </div>
     </div>

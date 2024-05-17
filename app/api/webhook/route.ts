9127bcabe890dfe4a41e32fa54bc35ec8664c883
Mac Sampson <mac.sampson@proton.me>
2024-05-16 20:44:58 -0700

    listen for payment succeeded event + clean up

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,19 +1,19 @@
-import Stripe from "stripe"
-import { headers } from "next/headers"
-import { NextResponse } from "next/server"
+import Stripe from 'stripe'
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
 
-import { stripe } from "@/lib/stripe"
-import prismadb from "@/lib/prismadb"
+import { stripe } from '@/lib/stripe'
+import prismadb from '@/lib/prismadb'
 
-import shippoClient from "@/lib/shippo"
+import shippoClient from '@/lib/shippo'
 
-type ItemsObject = {
-  [productId: string]: number | { [variationId: string]: number }
-}
+// type ItemsObject = {
+//   [productId: string]: number | { [variationId: string]: number }
+// }
 
 export async function POST(req: Request) {
   const body = await req.text()
-  const signature = headers().get("Stripe-Signature") as string
+  const signature = headers().get('Stripe-Signature') as string
 
   let event: Stripe.Event
 
@@ -28,37 +28,46 @@ export async function POST(req: Request) {
   }
 
   const session = event.data.object as Stripe.Checkout.Session
-  const currency = session?.currency
+  // TODO: implement currency conversion
+  // const currency = session?.currency
+  const billingAddress = session?.customer_details?.address
 
-  const address = session?.customer_details?.address
-
-  const addressComponents = [
-    address?.line1,
-    address?.line2,
-    address?.city,
-    address?.state,
-    address?.postal_code,
-    address?.country,
+  const billingAddressComponents = [
+    billingAddress?.line1,
+    billingAddress?.line2,
+    billingAddress?.city,
+    billingAddress?.state,
+    billingAddress?.postal_code,
+    billingAddress?.country,
   ]
 
-  const addressString = addressComponents.filter((c) => c !== null).join(", ")
+  const addressString = billingAddressComponents
+    .filter((c) => c !== null)
+    .join(', ')
 
-  // console.log("webhook received")
-  // console.log(event)
+  // Listen for the checkout.session.completed event
+  if (event.type === 'checkout.session.completed') {
+    await prismadb.order.update({
+      where: {
+        id: session?.metadata?.orderId,
+      },
+      data: {
+        // isPaid: true,
+        billingAddress: addressString,
+        emailAddress: session?.customer_details?.email || '',
+        totalPrice: session.amount_total ? session.amount_total / 100 : 0,
+      },
+    })
+  }
 
-  if (event.type === "checkout.session.completed") {
-    const order = await prismadb.order.update({
+  // Listen for the payment_intent.succeeded event
+  if (event.type === 'checkout.session.async_payment_succeeded') {
+    await prismadb.order.update({
       where: {
         id: session?.metadata?.orderId,
       },
       data: {
         isPaid: true,
-        address: addressString,
-        emailAddress: session?.customer_details?.email || "",
-        totalPrice: session.amount_total ? session.amount_total / 100 : 0,
-      },
-      include: {
-        orderItems: true,
       },
     })
 
@@ -129,60 +138,59 @@ export async function POST(req: Request) {
 
     // update the quantity of each product or product variation in the order
     // console.log("order: ", order)
-    await Promise.all(
-      order.orderItems.map(async (orderItem) => {
-        if (orderItem.productVariationId) {
-          const productVariation = await prismadb.productVariation.findUnique({
-            where: {
-              id: orderItem.productVariationId,
-            },
-          })
+    // await Promise.all(
+    //   order.orderItems.map(async (orderItem) => {
+    //     if (orderItem.productVariationId) {
+    //       const productVariation = await prismadb.productVariation.findUnique({
+    //         where: {
+    //           id: orderItem.productVariationId,
+    //         },
+    //       })
 
-          if (productVariation) {
-            await prismadb.productVariation.update({
-              where: {
-                id: orderItem.productVariationId,
-              },
-              data: {
-                quantity: {
-                  decrement: orderItem.quantity,
-                },
-              },
-            })
+    //       if (productVariation) {
+    //         await prismadb.productVariation.update({
+    //           where: {
+    //             id: orderItem.productVariationId,
+    //           },
+    //           data: {
+    //             quantity: {
+    //               decrement: orderItem.quantity,
+    //             },
+    //           },
+    //         })
 
-            await prismadb.product.update({
-              where: {
-                id: orderItem.productId,
-              },
-              data: {
-                quantity: {
-                  decrement: orderItem.quantity,
-                },
-              },
-            })
-          }
-        } else {
-          const product = await prismadb.product.findUnique({
-            where: {
-              id: orderItem.productId,
-            },
-          })
+    //         await prismadb.product.update({
+    //           where: {
+    //             id: orderItem.productId,
+    //           },
+    //           data: {
+    //             quantity: {
+    //               decrement: orderItem.quantity,
+    //             },
+    //           },
+    //         })
+    //       }
+    //     } else {
+    //       const product = await prismadb.product.findUnique({
+    //         where: {
+    //           id: orderItem.productId,
+    //         },
+    //       })
 
-          if (product) {
-            await prismadb.product.update({
-              where: {
-                id: orderItem.productId,
-              },
-              data: {
-                quantity: product.quantity - orderItem.quantity,
-              },
-            })
-          }
-        }
-      })
-    )
+    //       if (product) {
+    //         await prismadb.product.update({
+    //           where: {
+    //             id: orderItem.productId,
+    //           },
+    //           data: {
+    //             quantity: product.quantity - orderItem.quantity,
+    //           },
+    //         })
+    //       }
+    //     }
+    //   })
+    // )
 
-    // TODO: create shipment with shippo
     const shippingID = session.shipping_cost?.shipping_rate
 
     const shippingRate = await stripe.shippingRates.retrieve(
@@ -192,13 +200,14 @@ export async function POST(req: Request) {
     // console.log("shipping id: ", shippingRate.metadata.id)
 
     try {
-      const transaction = shippoClient.transaction.create({
+      shippoClient.transaction.create({
         rate: shippingRate.metadata.id,
-        label_file_type: "PDF",
+        label_file_type: 'PDF',
         async: false,
       })
-      console.log(transaction)
+      // console.log(transaction)
     } catch (error) {
+      // TODO: create webhook to alert admin of failed shipping label creation
       console.log(error)
     }
   }

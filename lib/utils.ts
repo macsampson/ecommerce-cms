2a11b62fa03f20a8e5f1446d3a682e73c9713e8f
Mac Sampson <mac.sampson@proton.me>
2024-07-01 21:37:25 -0700

    refactor form for products and modularize

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 // const to convert currnecy to USD
-export const formatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
+export const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
 })
+
+export const formatPriceDisplay = (price: number) => {
+  return `$${price.toFixed(2)}`
+}
+
+export const parsePriceInput = (input: string) => {
+  return parseFloat(input.replace(/[^0-9.-]+/g, ''))
+}

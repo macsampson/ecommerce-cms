8cbc6f24af66922f584b20ff9c94594e849aadb9
Mac Sampson <mac.sampson@proton.me>
2024-07-10 18:52:05 -0700

    use prisma seeding/migrations

---
+++ prisma/migrations/20240710220558_primary_keys/migration.sql
@@ -0,0 +1,263 @@
+-- CreateTable
+CREATE TABLE "store" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "store_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "billboard" (
+    "id" TEXT NOT NULL,
+    "store_id" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "landing_page" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "billboard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "carousel_image" (
+    "id" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "store_id" TEXT NOT NULL,
+    "image_credit" TEXT NOT NULL DEFAULT '',
+
+    CONSTRAINT "carousel_image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "category" (
+    "id" TEXT NOT NULL,
+    "store_id" TEXT NOT NULL,
+    "billboard_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "size" (
+    "id" TEXT NOT NULL,
+    "store_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "size_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "color" (
+    "id" TEXT NOT NULL,
+    "store_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "color_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product" (
+    "id" TEXT NOT NULL,
+    "store_id" TEXT NOT NULL,
+    "category_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "is_featured" BOOLEAN NOT NULL DEFAULT false,
+    "is_archived" BOOLEAN NOT NULL DEFAULT false,
+    "size_id" TEXT,
+    "color_id" TEXT,
+    "quantity" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "description" TEXT NOT NULL,
+    "weight" DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product_variation" (
+    "id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "product_variation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bundle" (
+    "id" TEXT NOT NULL,
+    "min_quantity" INTEGER NOT NULL,
+    "discount" DECIMAL(5,2) NOT NULL DEFAULT 0.00,
+    "product_id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "bundle_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "image" (
+    "id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "credit" TEXT NOT NULL DEFAULT '',
+    "ordering" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order" (
+    "id" TEXT NOT NULL,
+    "store_id" TEXT NOT NULL,
+    "is_paid" BOOLEAN NOT NULL DEFAULT false,
+    "address" TEXT NOT NULL DEFAULT '',
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "total_price" DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    "shipping_address" TEXT NOT NULL DEFAULT '',
+    "email_address" TEXT NOT NULL DEFAULT '',
+    "is_abandoned" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order_item" (
+    "id" TEXT NOT NULL,
+    "order_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+    "product_variation_id" TEXT,
+    "price" DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    "name" TEXT NOT NULL DEFAULT '',
+    "weight" DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+
+    CONSTRAINT "order_item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "billboard_store_id_idx" ON "billboard"("store_id");
+
+-- CreateIndex
+CREATE INDEX "carousel_image_store_id_idx" ON "carousel_image"("store_id");
+
+-- CreateIndex
+CREATE INDEX "category_billboard_id_idx" ON "category"("billboard_id");
+
+-- CreateIndex
+CREATE INDEX "category_store_id_idx" ON "category"("store_id");
+
+-- CreateIndex
+CREATE INDEX "size_store_id_idx" ON "size"("store_id");
+
+-- CreateIndex
+CREATE INDEX "color_store_id_idx" ON "color"("store_id");
+
+-- CreateIndex
+CREATE INDEX "product_category_id_idx" ON "product"("category_id");
+
+-- CreateIndex
+CREATE INDEX "product_color_id_idx" ON "product"("color_id");
+
+-- CreateIndex
+CREATE INDEX "product_size_id_idx" ON "product"("size_id");
+
+-- CreateIndex
+CREATE INDEX "product_store_id_idx" ON "product"("store_id");
+
+-- CreateIndex
+CREATE INDEX "product_variation_product_id_idx" ON "product_variation"("product_id");
+
+-- CreateIndex
+CREATE INDEX "bundle_product_id_idx" ON "bundle"("product_id");
+
+-- CreateIndex
+CREATE INDEX "image_product_id_idx" ON "image"("product_id");
+
+-- CreateIndex
+CREATE INDEX "order_store_id_idx" ON "order"("store_id");
+
+-- CreateIndex
+CREATE INDEX "order_item_order_id_idx" ON "order_item"("order_id");
+
+-- CreateIndex
+CREATE INDEX "order_item_product_id_idx" ON "order_item"("product_id");
+
+-- CreateIndex
+CREATE INDEX "order_item_product_variation_id_idx" ON "order_item"("product_variation_id");
+
+-- AddForeignKey
+ALTER TABLE "billboard" ADD CONSTRAINT "billboard_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "carousel_image" ADD CONSTRAINT "carousel_image_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "category" ADD CONSTRAINT "category_billboard_id_fkey" FOREIGN KEY ("billboard_id") REFERENCES "billboard"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "category" ADD CONSTRAINT "category_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "size" ADD CONSTRAINT "size_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "color" ADD CONSTRAINT "color_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product" ADD CONSTRAINT "product_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product" ADD CONSTRAINT "product_color_id_fkey" FOREIGN KEY ("color_id") REFERENCES "color"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product" ADD CONSTRAINT "product_size_id_fkey" FOREIGN KEY ("size_id") REFERENCES "size"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product" ADD CONSTRAINT "product_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_variation" ADD CONSTRAINT "product_variation_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bundle" ADD CONSTRAINT "bundle_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "image" ADD CONSTRAINT "image_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_item" ADD CONSTRAINT "order_item_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_item" ADD CONSTRAINT "order_item_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_item" ADD CONSTRAINT "order_item_product_variation_id_fkey" FOREIGN KEY ("product_variation_id") REFERENCES "product_variation"("id") ON DELETE SET NULL ON UPDATE CASCADE;

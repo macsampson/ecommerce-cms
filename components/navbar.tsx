fae654bf2b8ce1baa90b0bae4fa3f82b09f6f2b1
Mac Sampson <mac.sampson@proton.me>
2025-06-16 21:13:59 -0700

    hide store-switcher

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = async () => {
     <div className="border-b">
       <div className="flex h-16 items-center justify-between px-2 sm:px-4">
         <div className="flex items-center space-x-2 sm:space-x-4">
-          <StoreSwitcher items={stores} />
+          {/* <StoreSwitcher items={stores} /> */}
           <MainNav className="hidden sm:flex mx-2 sm:mx-6" />
         </div>
         <div className="flex items-center space-x-2 sm:space-x-4">

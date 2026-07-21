0f729290563ee128fa9f8b320e9abb0107de62f5
Mackenzie Sampson <mackenzie.sampson@outlook.com>
2026-07-21 09:01:33 -0700

    Fix blank revenue chart (bump rechart) and React 19 ref warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${body.variable} ${display.variable} ${data.variable} font-body`}
       >
